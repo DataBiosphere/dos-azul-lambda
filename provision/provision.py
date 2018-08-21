81257a03a5c3d8eaced60d8f3531ac5409ea6a9f
Natan Lao <natanlao@users.noreply.github.com>
2018-08-20 18:06:19 -0700

    Provisioner improvements (#86)
    * Stop giving Travis access to every ES instance
    
    * Use default region instead of hardcoding

--- provision/provision.py
+++ provision/provision.py
@@ -106,13 +106,15 @@ def setup(es_domain=None):
     if not es_domain:
         es_domain = 'dos-azul-test-' + str(uuid.uuid1()).split('-')[0]
     aws_info = boto3.client('sts').get_caller_identity()
-    # The access policy has curly braces so we can't use str.format()
-    # https://github.com/DataBiosphere/azul/blob/feature/commons/indexer/policies/elasticsearch-policy.json
+    region = boto3.session.Session().region_name
+
+    # Generate the policy from policy.json, filling in the blanks.
     with open(getpath('policy.json'), 'r') as ES_ACCESS_POLICY:
         policy = string.Template(ES_ACCESS_POLICY.read()).substitute({
             'aws_id': aws_info['Account'],
             'es_domain': es_domain,
-            'user_arn': aws_info['Arn']
+            'user_arn': aws_info['Arn'],
+            'region': region
         })
     es.create_elasticsearch_domain(
         DomainName=es_domain,
